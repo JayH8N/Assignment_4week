5bec153c9ff2e672f1c95782e9646a3052d113f9
JayH8N <dbwjdgns919@naver.com>
2023-08-08 14:49:54 +0900

    [ADD] : SwiftyJSON추가

---
+++ Assignment_4week/Lottery API/LottoViewController.swift
@@ -0,0 +1,62 @@
+//
+//  LottoViewController.swift
+//  Assignment_4week
+//
+//  Created by hoon on 2023/08/08.
+//
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+
+class LottoViewController: UIViewController {
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=1079"
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+extension UIViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        <#code#>
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        <#code#>
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        <#code#>
+    }
+    
+    
+}
