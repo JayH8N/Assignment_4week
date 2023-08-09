6906dbc52b1bf88d03e44e48effa9c26deb6123c
JayH8N <dbwjdgns919@naver.com>
2023-08-09 19:16:06 +0900

    [ADD] : searchBar 연결

--- Assignment_4week/Bookworm/BookwormCollectionViewCell.swift
+++ Assignment_4week/Bookworm/BookwormCollectionViewCell.swift
@@ -14,17 +14,30 @@ class BookwormCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet var bookTitle: UILabel!
     @IBOutlet var bookAuthor: UILabel!
-    @IBOutlet var likeButton: UIButton!
     @IBOutlet var bookImage: UIImageView!
     
     
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setCellStyle()
+        
     }
     
-    
-    
+    func setCellStyle() {
+        bookTitle.font = .boldSystemFont(ofSize: 15)
+        bookTitle.layer.addBorder([.bottom], width: 0.3, color: UIColor.black.cgColor)
+        bookAuthor.font = .systemFont(ofSize: 10)
+        bookAuthor.layer.addBorder([.bottom], width: 0.3, color: UIColor.black.cgColor)
+        bookImage.layer.cornerRadius = 10
+        bookImage.layer.shadowColor = UIColor.black.cgColor   //그림자 색깔
+        bookImage.layer.shadowOffset = CGSize(width: 0, height: 0)  //태양이 보는 시점
+        bookImage.layer.shadowRadius = 10  //그림자 코너깎임정도
+        bookImage.layer.shadowOpacity = 0.5   //그림자 투명도
+
+
+        //기본적으로 UIView의 clipsToBounds의 default값은 false다.
+        bookImage.clipsToBounds = true //false면 View가 깍이지 않게되고 true면 깍인다.
+    }
     
 }
--- Assignment_4week/Bookworm/BookwormViewController.swift
+++ Assignment_4week/Bookworm/BookwormViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SwiftyJSON
 import Alamofire
+import Kingfisher
 
 class BookwormViewController: UIViewController {
 
@@ -15,22 +16,53 @@ class BookwormViewController: UIViewController {
     
     var bookList: [BookData] = []
     
+    
+    @IBOutlet var searchBar: UISearchBar!
+    
     @IBOutlet var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         registerNib()
         collectionView.dataSource = self
         collectionView.delegate = self
-        
+        //callRequest(query: "아이유")
+        configureCollectionViewlayout()
+        searchBar.delegate = self
     }
     
-    func callRequest() {
-        let url = "https://dapi.kakao.com/v3/search/book"
-        AF.request(url, method: .get).validate().responseJSON { response in
+    func configureCollectionViewlayout() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical  //.horizontal: 수평, .vertical: 수직
+        layout.itemSize = CGSize(width: 120, height: 160) // 셀사이즈
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 0, right: 10) // CollectionView의 상하좌우 여백
+        layout.minimumLineSpacing = 20
+
+        collectionView.collectionViewLayout = layout
+    }
+    
+    func callRequest(query: String) {
+        let text = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        let url = "https://dapi.kakao.com/v3/search/book?query=\(text)"
+        let header: HTTPHeaders = ["Authorization": "KakaoAK \(APIKey.kakakoBook.rawValue)"]
+        AF.request(url, method: .get, headers: header).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-                print("JSON: \(json)")
+                //print("JSON: \(json)")
+                
+                
+                for item in json["documents"].arrayValue {
+                    let title = item["title"].stringValue
+                    let author = item["authors"][0].stringValue
+                    let cover = item["thumbnail"].stringValue
+                    
+                    
+                    let data = BookData(title: title, author: author, cover: cover)
+                    self.bookList.append(data)
+                }
+                
+                print(self.bookList)
+                
             case .failure(let error):
                 print(error)
             }
@@ -49,10 +81,29 @@ extension BookwormViewController: UICollectionViewDelegate, UICollectionViewData
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return bookList.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BookwormCollectionViewCell.identifier, for: indexPath) as? BookwormCollectionViewCell else { return UICollectionViewCell()}
+        
+        cell.backgroundColor = .lightGray
+        cell.bookTitle.text = bookList[indexPath.item].title
+        cell.bookAuthor.text = bookList[indexPath.item].author
+        if let url = URL(string: bookList[indexPath.item].cover) {
+            cell.bookImage.kf.setImage(with: url)
+        }
+        return cell
     }
-    
-    
+
+
+}
+
+extension BookwormViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        //page = 1 //검색시 마다 page를 1로 초기화 해야된다., 새로운 검색어이기 때문에 page를 1로 변경
+        bookList.removeAll()
+        
+        guard let query = searchBar.text else { return }
+        callRequest(query: query)
+    }
 }
