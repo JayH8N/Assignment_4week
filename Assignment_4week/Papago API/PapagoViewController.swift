a37922b43f09bc7bbf38ce6a6caf17f4bb076f1f
JayH8N <dbwjdgns919@naver.com>
2023-08-10 15:20:20 +0900

    [ADD] : Papago API 폴더 생성

---
+++ Assignment_4week/Papago API/PapagoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PapagoViewController.swift
+//  Assignment_4week
+//
+//  Created by hoon on 2023/08/10.
+//
+
+import UIKit
+
+class PapagoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
